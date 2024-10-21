e03f157a3e83a19ce75f148df0d1fe1a68e4e845
Sherif Yasser <sherifyasser92@gmail.com>
2024-10-21 20:53:33 +0200

    Removed some un-necessary comments

--- Castle Defenders/Castle Defenders/Gold/GoldNode.swift
+++ Castle Defenders/Castle Defenders/Gold/GoldNode.swift
@@ -10,8 +10,8 @@ class GoldNode : SKSpriteNode {
     var amount: Int!
     init(position: CGPoint, minValue : Int, maxValue : Int) {
         let goldImage = UIImage(systemName: "dollarsign.ring.dashed")!
-        let texture = SKTexture(image: goldImage) // Make sure to have a gold coin asset
-        let size = CGSize(width: 30, height: 30) // Adjust the size as needed
+        let texture = SKTexture(image: goldImage)
+        let size = CGSize(width: 30, height: 30) 
         amount = Int.random(in: minValue ... maxValue)
         super.init(texture: texture, color: .clear, size: size)
         self.zPosition = elementsZPos.goldDrops
